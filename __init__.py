7bd376a01cb7470dce9dc500ef6707b440f7ac42
drekorig <ebalexandr@gmail.com>
2024-04-04 10:19:11 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,37 @@
+from addon import MeshMergeSelectedVertices
+
+# Plugin registration
+
+
+def merge_vertices_button(self, context):
+    """Add merge vertices button"""
+    self.layout.operator(
+        MeshMergeSelectedVertices.bl_idname,
+        text="By Selection")
+
+
+def merge_vertices_manual_map():
+    """This allows you to right click on a button and link to documentation"""
+    url_manual_prefix = "https://github.com/drekorig/MeshMergeSelectedVertices/"
+    url_manual_mapping = (
+        ("bpy.ops.mesh.add_object", "blob/main/README.md"),
+    )
+    return url_manual_prefix, url_manual_mapping
+
+
+def register():
+    """Register addon"""
+    bpy.utils.register_class(MeshMergeSelectedVertices)
+    bpy.utils.register_manual_map(merge_vertices_manual_map)
+    bpy.types.VIEW3D_MT_edit_mesh_merge.append(merge_vertices_button)
+
+
+def unregister():
+    """Unregister addon"""
+    bpy.utils.unregister_class(MeshMergeSelectedVertices)
+    bpy.utils.unregister_manual_map(merge_vertices_manual_map)
+    bpy.types.VIEW3D_MT_edit_mesh_merge.remove(merge_vertices_button)
+
+
+if __name__ == "__main__":
+    register()
